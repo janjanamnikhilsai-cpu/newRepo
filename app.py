75c70b8f46c05cc2176f74fee15cd33091705e87
root <root@ip-172-31-13-65.ap-south-1.compute.internal>
2025-11-06 10:38:06 +0000

    py sample code

---
+++ app.py
@@ -0,0 +1,31 @@
+# Program to store and display student details
+
+print("===== Student Details Form =====")
+
+# Taking inputs
+name = input("Enter Student Name: ")
+age = int(input("Enter Age: "))
+course = input("Enter Course: ")
+college = input("Enter College Name: ")
+city = input("Enter City: ")
+
+# Taking marks
+print("\nEnter marks for 3 subjects:")
+sub1 = int(input("Subject 1: "))
+sub2 = int(input("Subject 2: "))
+sub3 = int(input("Subject 3: "))
+
+# Calculating total and percentage
+total = sub1 + sub2 + sub3
+percentage = total / 3
+
+# Displaying Output
+print("\n===== Student Report =====")
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("College:", college)
+print("City:", city)
+print("Total Marks:", total)
+print("Percentage:", percentage, "%")
+
